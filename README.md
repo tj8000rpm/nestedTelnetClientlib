d02fb63048e07d1eba7cf46113e125b6cd871181
Hiroshi Tsuji <t.j.8000rpm@gmail.com>
2019-01-10 01:06:29 +0900

    rename function, add README, add docstring, add comments

---
+++ README.md
@@ -0,0 +1,35 @@
+# nestedTelnetClientlib/NestedTelnetClient
+
+- auther: @tj8000rpm
+- repository: https://github.com/tj8000rpm/nestedTelnetClientlib
+
+
+## 目的（独断と偏見による）
+- 超レガシーな運用をしている人たちには*未だに*(そしてオリンピック以後の当分の間) ``telnet`` は重要なアイテムだ。
+- しかも超レガシーな設備は往々にして``ip reachability``が存在しないノードが存在しうる
+- しかしながら、超レガシーな環境にもかかわらず``自動化``と``AI化``が求められる！（なんてこった）
+- そんな超レガシーな運用の現場における``scripting``による踏み台との戦い、``expect``や``teraterm macro``に疲れた人のために踏み台がいくつあっても怖くないPython telnetlibのラッパーライブラリが必要だ。
+
+## 対象
+- あまり難しいことはしない。
+- とにかくtelnetの踏み台地獄と戦う人向け。
+- 踏み台の先にあるサーバでコマンドを実行し、その結果をパースしながら順次コマンドを打つみたいな用途で使う想定
+- なんか色々やりたい、冪等性ががが！となった人は``ansible``やその他WFを組めるような自動化ツールをお使いください。
+
+## 使い方
+- ``sample.py``を主に参照。
+- static methodである``connectWithTelnet``, ``writeACommandAndGetResultSTDOUT``だけを使えばOK。
+- 最初の踏み台サーバのみ,python独自のtelnetlibを用いる
+- それ以降の踏み台or最終目的サーバには``telnet``コマンドを順次打っていくだけです。
+
+1. 最初の踏み台サーバにアクセスするときのみ、``connectWithTelnet``の``telnetInstance``引数に``None``を指定するか、引数を指定しないでください。
+2. また戻り値である``telnetlib``のインスタンスを以後の踏み台or最終目的サーバに接続するまで``connectWithTelnet``の``telnetInstance``引数に指定し続けてください。
+3. 途中の踏み台を含め、コマンドを実行したい場合は``writeACommandAndGetResultSTDOUT``コマンドを用い、引数``telnetInstance``に上述の``telnetlib``インスタンスを指定してください。
+
+- その他の使い方は、``sample.py``を参照するか、チープな英語で書かれているソースコードの``docstirng``をご参照ください
+
+## 制限事項
+- rhel系/debian系/busyboxのtelnetサーバにしか今の所対応してないです。
+ - 正確には確認してないです
+- Unix系/switch系/交換機系等々の場合はカスタマイズが入りそうです
+- setup.pyとかは無いので自分でライブラリディレクトリに配置するか、自分のスクリプトの横において使用ください。
--- sample.py
+++ sample.py
@@ -1,21 +1,49 @@
 #!/usr/bin/python3
 from nestedTelnetClientlib import NestedTelnetClient
+'''This is an example of using NestedTelnetClient
 
+global config of nested telnet nodes
+below example config/code is the accessing to three depth nested telnet nodes.
+and run few commands in (1)node and (3)ndoe
+> client(this program) -telnet-> (1)192.168.122.44 -telnet-> (2)localhost -telnet-> (3)192.168.122.44
+
+exec order:
+client > 
+telnet(1)
+    (1)node >
+    uname -n
+    pwd
+    ip link | grep eth0
+    telnet(2)
+        (2)node >
+        telnet(3)
+            (3)node >
+            uname -n
+            exit
+        exit
+    exit
+'''
+
+# config as objects
 hosts=[
-    {'host':'10.0.0.1', 'user':'your_username', 'password':'your_password', 'prompt':':~$', 'command':[
+    {'host':'192.168.122.44', 'user':'root', 'password':'root123', 'prompt':'~ #', 'command':[
         'uname -n',
         'pwd',
         'ip link | grep eth0'
     ]},
-    {'host':'10.0.0.2', 'user':'your_username', 'password':'your_password', 'prompt':':~$', 'command':[
+    {'host':'localhost', 'user':'root', 'password':'root123', 'prompt':'~ #', 'command':[
     ]},
-    {'host':'10.0.0.3', 'user':'your_username', 'password':'your_password', 'prompt':':~$', 'command':[
+    {'host':'192.168.122.44', 'user':'root', 'password':'root123', 'prompt':'~ #', 'command':[
         'uname -n'
     ]}
 ]
 
+# this initialization is very important!
 tn=None
+
 exit_prompt=[]
+
+# connect and run commands each remote server according to above config
 for target in hosts:
     host=target['host']
     user=target['user']
@@ -23,13 +51,14 @@ for target in hosts:
     prompt=target['prompt']
     exit_prompt.insert(0, prompt)
     commands=target['command']
-    tn=NestedTelnetClient.loginServerWithTelnet(host, user, passwd, prompt, tn)
+    tn=NestedTelnetClient.connectWithTelnet((host, 23), user, passwd, prompt, tn)
     for command in commands:
-        outputs=NestedTelnetClient.sendMessage(tn, command, prompt)
+        outputs=NestedTelnetClient.writeACommandAndGetResultSTDOUT(tn, command, prompt)
         NestedTelnetClient.printOutputs(outputs)
 
+# graceful exit
 exit_prompt.append('')
 exit_prompt=exit_prompt[1:]
 for prompt in exit_prompt:
-    NestedTelnetClient.sendMessage(tn, 'exit', prompt)
+    NestedTelnetClient.writeACommandAndGetResultSTDOUT(tn, 'exit', prompt)
 
